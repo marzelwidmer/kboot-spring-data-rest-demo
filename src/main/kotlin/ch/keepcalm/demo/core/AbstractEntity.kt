8cb76ca7815bcf3670dc13d9fd690b90741ab7ae
Marcel Widmer <marzelwidmer@gmail.com>
2021-11-21 16:15:51 +0100

    Add in AbstractEntity Class CreatedDate with JsonProperty Configuration

--- src/main/kotlin/ch/keepcalm/demo/core/AbstractEntity.kt
+++ src/main/kotlin/ch/keepcalm/demo/core/AbstractEntity.kt
@@ -1,7 +1,8 @@
 package ch.keepcalm.demo.core
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import org.springframework.data.annotation.LastModifiedDate
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.data.annotation.CreatedDate
 import java.time.ZonedDateTime
 import javax.persistence.*
 
@@ -14,13 +15,18 @@ import javax.persistence.*
 open class AbstractEntity protected constructor() {
 
     @Id
-//    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     @JsonIgnore
     private val id: String? = null
+
+    @CreatedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private val date: ZonedDateTime? = null
+
 //    @Version
 //    private val version: String? = null
-    @LastModifiedDate
-    private val date: ZonedDateTime? = null
+//    @LastModifiedDate
+//    private val date: ZonedDateTime? = null
+
 }
